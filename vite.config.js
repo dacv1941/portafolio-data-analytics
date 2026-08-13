e4866dcf4d5b01f526004e38e1be2905c39bf950
dacv1941 <dacv1941@gmail.com>
2026-08-13 10:53:54 -0500

    Add files via upload
    Actualizo sitio con pagina de inicio y proyecto 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // IMPORTANTE: "base" debe ser "/NOMBRE-DE-TU-REPO/"
-// Si tu repositorio en GitHub se llama distinto a "portafolio-data-analytics",
-// cambia el valor de abajo por el nombre exacto de tu repositorio.
 export default defineConfig({
   plugins: [react()],
   base: "/portafolio-data-analytics/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        compensacion: resolve(__dirname, "compensacion.html"),
+        attrition: resolve(__dirname, "attrition.html"),
+      },
+    },
+  },
 });

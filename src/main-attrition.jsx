ab543bf3704b41c57809117b504d133ec761fb01
dacv1941 <dacv1941@gmail.com>
2026-08-13 10:56:12 -0500

    Add files via upload
    Agrego archivos del dashboard de attritionAgrego archivos del dashboard de attrition

---
+++ src/main-attrition.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AttritionDashboard from "./AttritionDashboard.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AttritionDashboard />
+  </React.StrictMode>
+);
